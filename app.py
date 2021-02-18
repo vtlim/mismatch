015c78f13a8a48a94c9691dec69965914c053a6f
Victoria Lim <lim.t.victoria@gmail.com>
2021-02-17 22:49:54 -0800

    simple form processing using ajax/jquery, change textarea focus color

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import sys
-from flask import Flask, request, render_template
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
-def my_form():
-    return render_template('main.html')
+def main():
+    return flask.render_template('main.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def my_form_post():
+@app.route('/process_sequences')
+def process_sequences():
 
-    if request.method == 'GET':
-        return render_template('main.html')
+    #form_data = request.form
+    #seqs_lhs = form_data["text_lhs"].split("\n")
+    #seqs_rhs = form_data["text_rhs"].split("\n")
+    #print("the LHS text is: ", seqs_lhs)
+    #print("the RHS text is: ", seqs_rhs)
+    #result = ""
+    #return render_template('main.html', result=result)
+    a = int(flask.request.args.get('text_lhs'))
+    b = int(flask.request.args.get('text_rhs'))
+    return flask.jsonify({"result_lhs": a+1, "result_rhs":b+1})
 
-    # if request is post
-    form_data = request.form
-    print("the LHS text is: ", form_data["text_lhs"])
-    print("the RHS text is: ", form_data["text_rhs"])
-    result = ""
-    return render_template('main.html', result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
